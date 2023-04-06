d6b540106649cb7f4eef00027434264668011846
jvhendrix <joaovitorsouza5000@gmail.com>
2023-04-06 16:57:27 -0300

    added comments and test empty input user password

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os
 from lib.pedido import Pedido
 
+
+# Função para escolher o tipo de interação:
+# Manual: O usuário digita os dados
+# Automatico: O usuário escolhe um arquivo de entrada
 def escolher_interacao(pedido):
     print("Escolha o modo de interacao:\n")
     print("Real")
@@ -26,7 +30,13 @@ def escolher_interacao(pedido):
         clear()
         print("Interacao invalida\n\n")
         return escolher_interacao(pedido)
-
+    
+    
+# Função equivalente ao estado inicial do automato (q0)
+# Neste estado, o usuário deve digitar seu nome de usuário
+# Qualquer conjunto de caracteres é aceito como nome de usuário exceto valores vazios
+# Caso o usuário digite um nome de usuário inválido, uma mensagem de erro é exibida
+# Para testar o estado de erro, basta digitar um nome de usuário vazio
 def estado_inicial(pedido):
     if pedido.interacao == "Automatico":
         pedido.usuario = pedido.entradas_arquivo[pedido.indice]
@@ -35,9 +45,19 @@ def estado_inicial(pedido):
     else:
         print("Bem vindo ao sistema de pedidos")
         pedido.usuario = input("Digite seu nome de usuário: ")
-    
-    return senha_inicial(pedido)
 
+    if not pedido.usuario:
+        print("\nNome de usuario invalido!\n")
+        return escolher_interacao(pedido)
+    else:
+        return senha_inicial(pedido)
+
+
+# Função equivalente ao segundo estado do automato (q1)
+# Neste estado, o usuário deve digitar sua senha
+# Qualquer conjunto de caracteres é aceito como senha exeto valores vazios
+# Caso o usuário digite uma senha inválida, uma mensagem de erro é exibida
+# Para testar o estado de erro, basta digitar uma senha vazia
 def senha_inicial(pedido):
     if pedido.interacao == "Automatico":
         pedido.senha = pedido.entradas_arquivo[pedido.indice]
@@ -45,8 +65,18 @@ def senha_inicial(pedido):
     else:
         pedido.senha = input("Digite sua senha: ")
         clear()
-    return qual_recheio(pedido)
+        
+    if not pedido.senha:
+        print("\nSenha invalida!\n")
+        return escolher_interacao(pedido)
+    else:
+        return qual_recheio(pedido)
 
+# Função equivalente ao terceiro estado do automato (q2)
+# Neste estado, o usuário deve escolher o recheio do sanduíche
+# Os recheios disponíveis são: Frango, Carne, Vegetariano
+# Caso o usuário digite um recheio inválido, uma mensagem de erro é exibida
+# Caso o usuário digite voltar, o estado inicial é chamado novamente
 def qual_recheio(pedido):
     if pedido.interacao == "Automatico":
         pedido.recheio = pedido.entradas_arquivo[pedido.indice]
@@ -72,7 +102,13 @@ def qual_recheio(pedido):
     else :
         clear()
         return adicional1(pedido)
-
+    
+# Função equivalente ao quarto estado do automato (q3)
+# Neste estado, o usuário deve escolher se deseja adicionar um recheio adicional
+# Caso o usuário escolha sim, o estado de adicionar recheio adicional (q4) é chamado
+# Caso o usuário escolha não, o estado de escolher salada (q5) é chamado
+# Caso o usuário escolha uma opção inválida, uma mensagem de erro é exibida
+# Caso o usuário escolha voltar, o estado de escolher recheio (q2) é chamado
 def adicional1(pedido): 
     if pedido.interacao == "Automatico":
         pedido.adicional1 = pedido.entradas_arquivo[pedido.indice]
@@ -97,6 +133,11 @@ def adicional1(pedido):
         print('Opção inválida')
         return escolher_interacao(pedido)
 
+# Função equivalente ao quinto estado do automato (q4)
+# Neste estado, o usuário deve escolher o recheio adicional do sanduíche
+# Os recheios disponíveis são: Frango, Carne, Vegetariano
+# Caso o usuário escolha um recheio inválido, uma mensagem de erro é exibida
+# Caso o usuário escolha "Cancelar adicional", o estado de escolher salada (q5) é chamado
 def adicional_recheio(pedido):
     if pedido.interacao == "Automatico":
         pedido.recheio_adicional = pedido.entradas_arquivo[pedido.indice]
@@ -123,6 +164,11 @@ def adicional_recheio(pedido):
         clear()
         return qual_salada(pedido)
 
+# Função equivalente ao sexto estado do automato (q5)
+# Neste estado, o usuário deve escolher a salada do sanduíche
+# As saladas disponíveis são: Alface, Tomate, Cenoura, Sem salada
+# Caso o usuário escolha uma salada inválida, uma mensagem de erro é exibida
+# Caso o usuário escolha voltar, o estado de escolher recheio (q2) é chamado
 def qual_salada(pedido):
     if pedido.interacao == "Automatico":
         pedido.salada = pedido.entradas_arquivo[pedido.indice]
@@ -150,6 +196,11 @@ def qual_salada(pedido):
         clear()
         return qual_molho(pedido)
 
+# Função equivalente ao sétimo estado do automato (q6)
+# Neste estado, o usuário deve escolher o molho do sanduíche
+# Os molhos disponíveis são: Maionese, Ketchup, Mostarda, Sem molho
+# Caso o usuário escolha um molho inválido, uma mensagem de erro é exibida
+# Caso o usuário escolha voltar, o estado de escolher salada (q5) é chamado
 def qual_molho(pedido):
     if pedido.interacao == "Automatico":
         pedido.molho = pedido.entradas_arquivo[pedido.indice]
@@ -176,7 +227,13 @@ def qual_molho(pedido):
     else :
         clear()
         return adicional2(pedido)
-
+    
+# Função equivalente ao oitavo estado do automato (q7)
+# Neste estado, o usuário deve escolher se deseja adicionar um molho adicional ao sanduíche
+# As opções disponíveis são: Sim, Não
+# Caso escolha "Sim", o estado de escolher molho adicional (q8) é chamado
+# Caso escolha "Não", o estado de escolher bebida (q9) é chamado
+# Caso o usuário escolha uma opção inválida, uma mensagem de erro é exibida
 def adicional2(pedido):
     if pedido.interacao == "Automatico":
         pedido.adicional2 = pedido.entradas_arquivo[pedido.indice]
@@ -201,6 +258,11 @@ def adicional2(pedido):
         print('Opção inválida')
         return escolher_interacao(pedido)
 
+# Função equivalente ao nono estado do automato (q8)
+# Neste estado, o usuário deve escolher o molho adicional do sanduíche
+# Os molhos disponíveis são: Maionese, Ketchup, Mostarda
+# Caso o usuário escolha um molho adicional inválido, uma mensagem de erro é exibida
+# Caso o usuário escolha "Cancelar adicional", o estado de escolher bebida (q9) é chamado
 def adicional_molho(pedido):
     if pedido.interacao == "Automatico":
         pedido.molho_adicional = pedido.entradas_arquivo[pedido.indice]
@@ -227,6 +289,11 @@ def adicional_molho(pedido):
         clear()
         return qual_bebida(pedido)
 
+# Função equivalente ao décimo estado do automato (q9)
+# Neste estado, o usuário deve escolher a bebida que acompanha sanduíche
+# As bebidas disponíveis são: Refrigerante, Suco, Agua
+# Caso o usuário escolha uma bebida inválida, uma mensagem de erro é exibida
+# Caso o usuário escolha voltar, o estado de escolher molho adicional (q8) é chamado
 def qual_bebida(pedido):
     if pedido.interacao == "Automatico":
         pedido.bebida = pedido.entradas_arquivo[pedido.indice]
@@ -253,6 +320,13 @@ def qual_bebida(pedido):
         clear()
         return confirmacao_pedido(pedido)
 
+# Função equivalente ao décimo primeiro estado do automato (q10)
+# Neste estado, o usuário deve escolher se deseja adicionar mais algum lanche ao seu pedido
+# As opções disponíveis são: Sim, Não, Voltar a editar o pedido
+# Caso escolha "Sim", o estado de escolher recheio (q2) é chamado
+# Caso escolha "Não", o estado de escolher forma de entrega (q11) é chamado
+# Caso escolha "Voltar a editar o pedido", o estado de escolher molho (q7) é chamado
+# Caso o usuário escolha uma opção inválida, uma mensagem de erro é exibida
 def confirmacao_pedido(pedido):
     if pedido.interacao == "Automatico":
         novo_pedido = pedido.entradas_arquivo[pedido.indice]
@@ -283,6 +357,12 @@ def confirmacao_pedido(pedido):
         print("Opção de adição de pedido inválida\n\n")
         return escolher_interacao(pedido)
 
+
+# Função equivalente ao décimo segundo estado do automato (q11)
+# Neste estado, o usuário deve escolher a forma de entrega do seu pedido
+# As formas de entrega disponíveis são: Retirar no balcão, Entrega
+# Caso o usuário escolha uma forma de entrega inválida, uma mensagem de erro é exibida
+# Caso o usuário escolha voltar, o estado de escolher forma de entrega (q10) é chamado
 def entrega(pedido):
     if pedido.interacao == "Automatico":
         pedido.forma_entrega = pedido.entradas_arquivo[pedido.indice]
@@ -308,6 +388,10 @@ def entrega(pedido):
         clear()
         return pagamento(pedido)
 
+# Função equivalente ao décimo terceiro estado do automato (q12)
+# Neste estado, o usuário deve digitar o número do cartão de crédito
+# Caso o usuário digite um número de cartão inválido, uma mensagem de erro é exibida e o usuário continua em q12
+# Caso o usuário digite um número de cartão válido, o estado de digitar senha do cartão (q13) é chamado
 def pagamento(pedido):
     if pedido.interacao == "Automatico":
         if pedido.indice < len(pedido.entradas_arquivo):
@@ -332,6 +416,14 @@ def pagamento(pedido):
         pedido.senha_cartao = pedido.cartao[-4:]
         return senha_cartao(pedido)
 
+
+# Função equivalente aos estados (q13, q14, q15 e q16) do automato 
+# Neste estado, o usuário deve digitar a senha do cartão de crédito
+# Caso o usuário digite uma senha inválida, uma mensagem de erro é exibida
+# Como o o usuario apenas pode digitar uma senha válida, para testar o estado de erro, basta digitar uma senha diferente dos ultimos 4 dígitos do cartão
+# Apos a mensagem de erro, o usuario é redirecionado para o proximo estado de insercao de senha do cartao (q13->q14->q15->q16)
+# Caso o usuário digite uma senha válida, o estado de imprimir sucesso no pedido (q17) é chamado
+# Caso o usuário digite uma senha inválida 4 vezes, será exibida uma mensagem de erro e o usuário será redirecionado para o estado de escolher interação (q1)
 def senha_cartao(pedido):
     
     if pedido.interacao == "Automatico":
@@ -364,6 +456,8 @@ def senha_cartao(pedido):
         print("Número de tentativas de senha excedido\n\n")
         return escolher_interacao(pedido)
 
+# Função equivalente ao décimo sétimo estado do automato (q17)
+# Neste estado, é exibida uma mensagem de sucesso no pedido e o usuário é redirecionado para o estado de escolher interação (q1)
 def imprime(pedido):
     print("=======================================================")
     print("=========== Pedido realizado com sucesso!! ============")
@@ -371,10 +465,11 @@ def imprime(pedido):
     print("=======================================================\n")
     
     return main()
-
+# Limpar o terminal
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+# Função de inicialização da aplicação e criação do objeto pedido
 def main():
     #clear()
     meupedido = Pedido()
